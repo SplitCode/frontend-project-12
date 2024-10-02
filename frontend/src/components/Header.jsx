85f39a70dd95404a84ce6230ff6da20ccddb2714
Natalia Bandurova <natacsp@mail.ru>
2024-10-02 23:55:50 +0300

    feat: add header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,14 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { NavLink } from 'react-router-dom';
+
 const Header = () => (
-  <div>
-    Header
-  </div>
+  <Navbar expand="lg" className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
+    <Container>
+      <Navbar.Brand as={NavLink} to="/">Hexlet Chat</Navbar.Brand>
+      <button type="button" className="btn btn-primary">Выйти</button>
+    </Container>
+  </Navbar>
 );
 
 export default Header;
