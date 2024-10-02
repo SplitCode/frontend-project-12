e5ec0294a9e9d48a55f286184aead28f4e6ec5d9
Natalia Bandurova <natacsp@mail.ru>
2024-10-03 01:20:23 +0300

    feat: add private router draft

---
+++ frontend/src/components/PrivateRoute.jsx
@@ -0,0 +1,12 @@
+// import { useLocation, Navigate } from 'react-router-dom';
+
+// const PrivateRoute = ({ children }) => {
+//   const auth = useAuth();
+//   const location = useLocation();
+
+//   return (
+//     auth.loggedIn ? children : <Navigate to="/login" state={{ from: location }} />
+//   );
+// };
+
+// export default PrivateRoute;
