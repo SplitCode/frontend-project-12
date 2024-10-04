1ab83bac3becec3218c22dd7dc923a4316a41b54
Natalia Bandurova <natacsp@mail.ru>
2024-10-04 12:12:45 +0300

    feat: add bootstrap icons

--- frontend/src/components/Channels.jsx
+++ frontend/src/components/Channels.jsx
@@ -1,6 +1,7 @@
 import Nav from 'react-bootstrap/Nav';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import { Plus } from 'react-bootstrap-icons';
 import { useGetChannelsQuery } from '../api/getChannels';
 
 const Channels = () => {
@@ -8,7 +9,7 @@ const Channels = () => {
   console.log(channels);
 
   return (
-    <Col md="2" xs="4" className="border-end px-0 bg-light flex-column h-100 d-flex">
+    <Col xs="4" md="2" className="border-end px-0 bg-light flex-column h-100 d-flex">
       <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
         <b>Каналы</b>
         <Button
@@ -16,10 +17,10 @@ const Channels = () => {
           variant="outline-primary"
           onClick={console.log('+')}
         >
-          +
+          <Plus />
         </Button>
       </div>
-      <Nav className="flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
+      <Nav className="flex-column nav-fill px-2 mb-3 overflow-auto h-100 d-block">
         <Nav.Item>
           <Button
             variant="secondary"
