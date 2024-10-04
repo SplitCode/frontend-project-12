f3a1b8ec728c212873b4e21333006b98deab2807
Natalia Bandurova <natacsp@mail.ru>
2024-10-04 12:57:12 +0300

    feat: add messagesApi to store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { channelsApi } from '../api/channelsApi';
+import { messagesApi } from '../api/messagesApi';
 
 import authReducer from './slices/authSlice';
 
@@ -7,8 +8,10 @@ const store = configureStore({
   reducer: {
     auth: authReducer,
     [channelsApi.reducerPath]: channelsApi.reducer,
+    [messagesApi.reducerPath]: messagesApi.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(channelsApi.middleware),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+    .concat(channelsApi.middleware, messagesApi.middleware),
 });
 
 export default store;
