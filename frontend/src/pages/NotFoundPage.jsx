dd6d4411780ac95df90f8b1fe7e0bfadbdd71ee8
Natalia Bandurova <natacsp@mail.ru>
2024-10-28 17:49:03 +0300

    fix: fix 404 page image

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import notFoundImage from '../assets/images/notFound.jpg';
+import notFoundImage from '../assets/images/notFound.svg';
 
 const NotFoundPage = () => {
   const { t } = useTranslation();
@@ -11,8 +11,6 @@ const NotFoundPage = () => {
         alt={t('notFoundPage.title')}
         className="img-fluid h-25"
         src={notFoundImage}
-        width="227"
-        height="227"
       />
       <h1 className="h4 text-muted">{t('notFoundPage.title')}</h1>
       <p className="text-muted">
