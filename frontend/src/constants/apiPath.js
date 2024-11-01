ded823f5610467c8d236b5e2aee1bd192a4a766c
Natalia Bandurova <natacsp@mail.ru>
2024-11-01 11:11:34 +0300

    refactor: change constant for api version

--- frontend/src/constants/apiPath.js
+++ frontend/src/constants/apiPath.js
@@ -1,15 +1,13 @@
 import PATHS from './paths';
 
-export const API_PREFIX = 'api';
-export const API_VERSION = 'v1';
-export const BASE_API_PATH = `/${API_PREFIX}/${API_VERSION}`;
+const API_V1 = 'api/v1';
 
 export const API_PATHS = {
   LOGIN: () => PATHS.LOGIN_PATH,
   SIGNUP: () => PATHS.SIGNUP_PATH,
-  CHANNELS: () => `${BASE_API_PATH}${PATHS.CHANNELS_PATH}`,
-  MESSAGES: () => `${BASE_API_PATH}${PATHS.MESSAGES_PATH}`,
-  BASE: () => BASE_API_PATH,
+  CHANNELS: () => `${API_V1}${PATHS.CHANNELS_PATH}`,
+  MESSAGES: () => `${API_V1}${PATHS.MESSAGES_PATH}`,
+  BASE: () => API_V1,
 };
 
 const getApiPath = (path) => API_PATHS[path]();
