74aaddbc11a35df0e909bb461af9e2413cbcf079
Natalia Bandurova <natacsp@mail.ru>
2024-11-01 21:01:19 +0300

    refactor: move sign up form to separate component

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom';
+import App from '../App';
+import PrivateRoute from './private-route';
 import ChatPage from '../pages/ChatPage';
 import LoginPage from '../pages/LoginPage/LoginPage';
 import NotFoundPage from '../pages/NotFoundPage';
-import App from '../App';
-import PrivateRoute from './private-route';
-import SignUpPage from '../pages/SignUpPage';
+import SignUpPage from '../pages/SignUpPage/SignUpPage';
 import getRoutesPath from '../constants/routesPath';
 
 const router = createBrowserRouter([
