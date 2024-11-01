cb3a69016344d79d8af57830a62d845ebef4b597
Natalia Bandurova <natacsp@mail.ru>
2024-11-01 13:16:50 +0300

    refactor: remove functions from paths

--- frontend/src/constants/apiPath.js
+++ frontend/src/constants/apiPath.js
@@ -3,13 +3,13 @@ import PATHS from './paths';
 const API_V1 = 'api/v1';
 
 export const API_PATHS = {
-  LOGIN: () => PATHS.LOGIN_PATH,
-  SIGNUP: () => PATHS.SIGNUP_PATH,
-  CHANNELS: () => `${API_V1}${PATHS.CHANNELS_PATH}`,
-  MESSAGES: () => `${API_V1}${PATHS.MESSAGES_PATH}`,
-  BASE: () => API_V1,
+  LOGIN: PATHS.LOGIN_PATH,
+  SIGNUP: PATHS.SIGNUP_PATH,
+  CHANNELS: `${API_V1}${PATHS.CHANNELS_PATH}`,
+  MESSAGES: `${API_V1}${PATHS.MESSAGES_PATH}`,
+  BASE: API_V1,
 };
 
-const getApiPath = (path) => API_PATHS[path]() || '';
+const getApiPath = (path) => API_PATHS[path] || '';
 
 export default getApiPath;
