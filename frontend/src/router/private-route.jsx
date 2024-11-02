7f9020ef4a04d92349e84f46ddbc61ab37f76042
Natalia Bandurova <natacsp@mail.ru>
2024-11-02 15:54:41 +0300

    fix: fix private route

--- frontend/src/router/private-route.jsx
+++ frontend/src/router/private-route.jsx
@@ -1,14 +1,14 @@
 import { useLocation, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { selectToken } from '../store/slices/authSlice';
+import { selectIsAuth } from '../store/slices/authSlice';
 import { LOGIN_PATH, getRoutesPath } from './routesPath';
 
 const PrivateRoute = ({ element }) => {
-  const token = useSelector(selectToken);
+  const isAuth = useSelector(selectIsAuth);
   const location = useLocation();
 
   return (
-    token ? element : <Navigate to={getRoutesPath(LOGIN_PATH)} state={{ from: location }} />
+    isAuth ? element : <Navigate to={getRoutesPath(LOGIN_PATH)} state={{ from: location }} />
   );
 };
 
